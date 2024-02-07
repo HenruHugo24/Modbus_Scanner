9e2119e735374f4d75ba1299165f3d9e2447a019
HenruHugo24 <henru.hugo@gmail.com>
2024-02-07 11:21:12 +0200

    Made it clean

--- Modbus.go
+++ Modbus.go
@@ -11,7 +11,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/c-robinson/iplib/v2"
+	"github.com/c-robinson/iplib"
 	"github.com/goburrow/modbus"
 )
 
@@ -19,18 +19,18 @@ import (
 type Config struct {
 	Protocol           string        `json:"protocol"`
 	IPmask             string        `json:"ip_mask"`
-	IPDevice           string        `json:"ip_device"`
+	IPDevice           string        `json:"gateway"`
 	SlaveIDRange       IDRange       `json:"slave_id_range"`
 	KnownRegisterRange RegisterRange `json:"known_register_range"`
-	LengthOfEachRead   int           `json:"length_of_each_read"`
+	LengthOfRead       int           `json:"length_of_each_read"`
 	Port               string        `json:"port"`
 	FunctionCode       int           `json:"function_code"`
 	BaudRates          []int         `json:"baud_rates"`
 	Endianness         string        `json:"endianness"`
 }
 type RegisterRange struct {
-	first int `json:"startvalue"`
-	last  int `json:"endvalue"`
+	First int `json:"startvalue"`
+	Last  int `json:"endvalue"`
 }
 
 // IDRange struct to hold start and end IDs
@@ -39,121 +39,116 @@ type IDRange struct {
 	End   int `json:"end"`
 }
 
-func loadjson(filename string) (Config, error) {
+func loadjson(filename string) (Config, error) { //youtube
 	var config Config
 	configfile, err := os.Open(filename)
 	if err != nil {
 		return config, err
 	}
-	defer configfile.Close()
+
 	jsonParser := json.NewDecoder(configfile)
 	err = jsonParser.Decode(&config)
+	defer configfile.Close()
 	return config, err
 }
 
 // ////////////////////////////Start of MAIN//////////////////////////////////////////////
 func main() {
-	//Load json file
+	// Load json file
 	json_data, _ := loadjson("config.json")
-	check_IP_connection("10.6.70.5", "502")
-	value := json_data.LengthOfEachRead
-	fmt.Println(value)
-	//Actual code
-	ip_mask := convert_IP(json_data.IPmask)
-	ip_device := convert_IP(json_data.IPDevice)
-	Total_adresses := ip_mask ^ (0b11111111111111111111111111111111)
-	fmt.Printf("Total number of adresses to scan: %d\n", Total_adresses)
-	ip_counter := ip_mask & ip_device
+
+	// couple of importantvariables
 	port_number := json_data.Port
+	register_length := json_data.LengthOfRead
+	register_start_byte := json_data.KnownRegisterRange.First
+	register_end_byte := json_data.KnownRegisterRange.Last
+	amount_of_registers := register_end_byte - register_start_byte
+	fmt.Printf("Length of read %d\nFrom register %d\nTo register %d\n", register_length, register_start_byte, register_end_byte)
+
+	//Starting IP and amount of addresses to read
+	ip_mask := convert_IP(json_data.IPmask)
+	gateway := convert_IP(json_data.IPDevice) // if gateway can be the device it would be nice
+	Total_adresses := ip_mask ^ (0b11111111111111111111111111111111)
+	ip_counter := ip_mask & gateway
 
 	for i := 1; i <= Total_adresses; i++ {
 		// fmt.Printf("%b\n", ip_counter+i)
 		ip_checker := iplib.Uint32ToIP4(uint32((ip_counter) + i))
 		bool_port_connection := check_IP_connection(ip_checker.String(), port_number)
 
-		if bool_port_connection && (ip_counter != ip_device) { // check if it is the Host
+		if bool_port_connection && (ip_counter != gateway) { // check if it is the Host
 			for j := json_data.SlaveIDRange.Start; j <= json_data.SlaveIDRange.End; j++ {
-				data, err := modbusmaker(ip_checker.String(), byte(j), 101, 1)
-				fmt.Printf(ip_checker.String()+" %v %d\n", data, err)
+				for k := 0; k < amount_of_registers; k += register_length {
+					data, _, fail := modbusmaker(ip_checker.String(), byte(j), uint16(register_start_byte+k), uint16(register_length))
+					if fail == 0 {
+						fmt.Printf("IP addres "+ip_checker.String()+" SlaveID %d Register Data %v\n", j, data)
+					}
+
+				}
+
 			}
 
 		}
-		// if !bool_port_connection {
-		// 	fmt.Printf("number %d the data is %v\n", i, "lost")
-		// }
 	}
-	fmt.Println("Made it")
-	//connect to the deepsea
-	fuel_level, _ := modbusmaker("10.6.70.5", 0, 1027, 1)
-	fmt.Printf("Read Fuel level of deepsea: %d%%\n", fuel_level[1])
+	fmt.Println("End of search hope you found what you are looking for")
+	// 	//Deepsea "10.6.70.5" Fuel level 1027
+	// 	fuel_level, _, _ := modbusmaker("10.6.70.5", 0, 1027, 1)
+	// 	fmt.Printf("Read Fuel level of deepsea: %d%%\n", fuel_level[1])
 
-	//Connect to new Bluelog
-	// Bluelog `10.6.70.15` Power = 254, Freq = 98, Voltage = 100
-	power_bluelog, _ := modbusmaker("10.6.70.15", 1, 254, 2)
-	fmt.Printf("Read Power of bluelog: %v\n", bytesToFloat32(power_bluelog))
+	// 	// Bluelog `10.6.70.15` Power = 254, Freq = 98, Voltage = 100
+	// 	power_bluelog, _, _ := modbusmaker("10.6.70.15", 1, 254, 2)
+	// 	fmt.Printf("Read Power of bluelog: %v\n", bytesToFloat32(power_bluelog))
 
-	// SMA Inverter `10.6.70.28` Power = 199, Freq = 201
-	power_SMA, _ := modbusmaker("10.6.70.28", 0, 199, 2)
-	fmt.Printf("Read power of SMA %v\n", power_SMA)
+	// 	// SMA Inverter `10.6.70.28` Power = 199, Freq = 201
+	// 	power_SMA, _, _ := modbusmaker("10.6.70.28", 0, 199, 2)
+	// 	fmt.Printf("Read power of SMA %v\n", power_SMA)
 }
 
 ///////////////////////////////////END OF MAIN//////////////////////////////////////////////////////////////
 
-func bytesToFloat32(bytes []byte) float32 {
-	// Assuming Big-endian byte order, adjust accordingly if needed
+func bytesToFloat32(bytes []byte) float32 { //not sure if this works
 	bits := binary.BigEndian.Uint32(bytes)
 	return math.Float32frombits(bits)
 }
 
-func modbusmaker(IP_mask string, slaveID byte, register_value uint16, number_bytes uint16) ([]byte, int) {
-	// i = 1 connection i=2 read and i=3 both error
+func modbusmaker(IP_mask string, slaveID byte, register_value uint16, number_bytes uint16) ([]byte, error, int) { //his is the man doing the talking
+	// i = 1 connection error i=2 read error and i=3 both error
 	i := 0
 	address := IP_mask + ":502"
 	handler := modbus.NewTCPClientHandler(address)
 
-	handler.Timeout = time.Second / 10 // Set your timeout value
+	handler.Timeout = time.Second / 10
 	handler.SlaveId = slaveID
-	// Deepsea `10.6.70.5`
 
-	// Create Modbus client using the handler
 	client := modbus.NewClient(handler)
 
-	// Connect to the Modbus server or return a error message
 	err := handler.Connect()
 	if err != nil {
-		fmt.Println(err)
-		// fmt.Println("Connection error")
 		i++
 	}
 	defer handler.Close()
 
 	results, err := client.ReadHoldingRegisters(register_value, number_bytes)
 	if err != nil {
-		fmt.Println(err)
-		// fmt.Println("Reading error")
-		i += 2
+		i = i + 2
 	}
-	return results, i
-	// Display the results
-	// fmt.Printf("Read Fuel level of deepsea: %d%%\n", results[1])
+	return results, err, i
 }
 
 func check_IP_connection(host string, port string) bool {
 	timeout := time.Second / 50
-	conn, err := net.DialTimeout("tcp", net.JoinHostPort(host, port), timeout)
+	conn, err := net.DialTimeout("tcp", net.JoinHostPort(host, port), timeout) //not sure thank you google
 	boolean := false
 	if err != nil {
-		// fmt.Print("Closed\n")
 		boolean = false
 	}
 	if conn != nil {
-		// fmt.Print("Open\n")
 		boolean = true
 	}
 	return boolean
 }
 
-func convert_IP(IP string) int {
+func convert_IP(IP string) int { //convert from string to integer
 	octets := strings.Split(IP, ".")
 	octet0, _ := strconv.Atoi(octets[0])
 	octet1, _ := strconv.Atoi(octets[1])
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.21.6
 require github.com/goburrow/modbus v0.1.0
 
 require (
+	github.com/c-robinson/iplib v1.0.8
 	github.com/c-robinson/iplib/v2 v2.0.4 // indirect
 	github.com/goburrow/serial v0.1.0 // indirect
 	lukechampine.com/uint128 v1.3.0 // indirect
